2c5504415d07ccd306ae739e857047f39f3ed249
Bruno Guimarães <brg.brunorocha@gmail.com>
2024-06-02 14:27:26 -0300

    Q7

--- Q7/main.c
+++ Q7/main.c
@@ -1,1 +1,37 @@
 // Feito por everton riberio
+
+#include <stdio.h>
+#include <math.h>
+
+typedef struct {
+    double raio;
+    double argumento; 
+} CoordenadaPolar;
+
+typedef struct {
+    double x;
+    double y;
+} CoordenadaCartesiana;
+
+CoordenadaCartesiana converteParaCartesiana(CoordenadaPolar polar) {
+    CoordenadaCartesiana cartesiana;
+    cartesiana.x = polar.raio * cos(polar.argumento);
+    cartesiana.y = polar.raio * sin(polar.argumento);
+    return cartesiana;
+}
+
+int main() {
+    CoordenadaPolar pontoPolar;
+    CoordenadaCartesiana pontoCartesiano;
+
+    printf("Digite o raio: ");
+    scanf("%lf", &pontoPolar.raio);
+    printf("Digite o argumento (em radianos): ");
+    scanf("%lf", &pontoPolar.argumento);
+
+    pontoCartesiano = converteParaCartesiana(pontoPolar);
+
+    printf("Coordenadas cartesiana: x = %.2f, y = %.2f\n", pontoCartesiano.x, pontoCartesiano.y);
+
+    return 0;
+}
